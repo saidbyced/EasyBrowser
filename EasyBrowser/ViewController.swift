cbba377d7c7aa84a122c1533ced1566e4ed543a0
Chris Eadie <chris.eadie@outlook.com>
2020-09-24 14:58:58 +0100

    Restrict loading of websites to array of websites

--- EasyBrowser/ViewController.swift
+++ EasyBrowser/ViewController.swift
@@ -66,4 +66,18 @@ class ViewController: UIViewController, WKNavigationDelegate {
         title = webView.title
     }
     
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+        
+        if let host = url?.host {
+            for website in websites {
+                if host.contains(website) {
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+            
+            decisionHandler(.cancel)
+        }
+    }
 }
